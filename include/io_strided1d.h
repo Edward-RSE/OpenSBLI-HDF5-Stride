760ab6c4e95912b0cd4e9ee4b2a5af9324b15f69
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-10-18 15:09:19 +0100

    need to divide by stride_i, otherwise index_new is too big

--- include/io_strided1d.h
+++ include/io_strided1d.h
@@ -7,17 +7,17 @@ ops_dat create_strided_ops_dat(ops_dat dat, size_t stride_i) {
 
   int d_p[] = {0};
   int d_m[] = {0};
-  int new_size[] = {ceil(block0np0 / stride_i)};
+  int new_size[] = {(int)ceil(block0np0 / stride_i)};
 
   double *_dummy = NULL; /* Note that this has to be the type you want the ops_dat to be */
   ops_dat new_dat = ops_decl_dat(dat->block, dat->dim, new_size, dat->base, d_m, d_p, _dummy, dat->type, dat->name);
 
   int size[] = {dat->size[0]};
-  int offset[] = {abs(dat->d_m[0])};
+  int offset[] = {(int)abs(dat->d_m[0])};
 
   for (int i = offset[0]; i < size[0] - dat->d_p[0]; i += stride_i) {
     const size_t index_dat = i * dat->elem_size;
-    const size_t index_new = (i - offset[0]) * new_dat->elem_size;
+    const size_t index_new = (i - offset[0]) * new_dat->elem_size / stride_i;
     memcpy(new_dat->data + index_new, dat->data + index_dat, new_dat->elem_size);
   }
 
