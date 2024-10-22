a4a912d076aa10b5dcc0b44927783c8dc2a867a9
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-10-22 13:21:08 +0100

    doc and clean up for 1d and 2d striding

--- include/io_strided1d.h
+++ include/io_strided1d.h
@@ -1,50 +1,173 @@
-/*
- * Takes an input dataset to create a new strided dataset
- * In this example, we create a new ops_dat without any halo cells, which
- * significantly simplifies copying data between buffers.
+/**
+ * @brief Convert between 1D indices and linear indices
+ *
+ * @param[in] i The i index
+ * @param[in] size The size of each dimension (unused)
+ *
+ * @returns The linear index
+ *
  */
-ops_dat create_strided_ops_dat(ops_dat dat, size_t stride_i) {
-  int d_p[] = {0};
-  int d_m[] = {0};
-  int new_size[] = {(int)ceil(block0np0 / stride_i)};
+size_t index1d(size_t i, int *size) { return i; }
 
-  double *_dummy = NULL; /* Note that this has to be the type you want the ops_dat to be */
-  ops_dat new_dat = ops_decl_dat(dat->block, dat->dim, new_size, dat->base, d_m, d_p, _dummy, dat->type, dat->name);
+/**
+ * @brief Creates a strided ops_dat, without halo cells, from an existing
+ *        ops_dat.
+ *
+ * @param[in] dat The ops_dat to create a strided ops_dat from
+ * @param[in] stride_i The stride in the i direction
+ *
+ * @returns The strided ops_dat
+ *
+ * @details
+ *
+ * This function creates a strided ops_dat by taking an existing ops_dat and
+ * creating a new ops_dat with the specified stride in each dimension. There
+ * are no halo cells, because it simplifies copying data between buffers.
+ *
+ */
+ops_dat create_strided_ops_dat_without_halo_cells(ops_dat dat, size_t stride_i) {
+  /*
+   * Determine the size of the strided dat halos and the actual size of the
+   * dataset. In this case, we do not have any halo cells.
+   */
+  int strided_d_p[] = {0};
+  int strided_d_m[] = {0};
+  int strided_size[] = {(int)ceil(block0np0 / stride_i)};
 
-  int size[] = {dat->size[0]};
-  int offset[] = {(int)abs(dat->d_m[0])};
+  /*
+   * Create a new dataset using the sizes above. We need to create a new ops_dat
+   * rather than just a buffer since all the HDF5 functions (which we want to
+   * re-use) expect an ops_dat and not a buffer containing data. Note that we
+   * have a dummy variable. This variable *HAS* to be the type of data which
+   * will be stored in the ops_dat. If it is not, this will allocate an
+   * incorrect amount of data and will also initialise some of the fields, such
+   * as ops_dat->elem_size, incorrectly. Anything wrong there will result in
+   * copy errors and garbage output.
+   */
+  double *dummy = NULL;
+  ops_dat strided_dat = ops_decl_dat(dat->block, dat->dim, strided_size, dat->base, strided_d_m, strided_d_p, dummy,
+                                     dat->type, dat->name);
 
-  for (int i = offset[0]; i < size[0] - dat->d_p[0]; i += stride_i) {
-    const size_t index_dat = i * dat->elem_size;
-    const size_t index_new = (i - offset[0]) * new_dat->elem_size / stride_i;
-    memcpy(new_dat->data + index_new, dat->data + index_dat, new_dat->elem_size);
+  /*
+   * Loop variables to control the start and stop of the loop over each
+   * dimension. These values are based on the size of the original dataset. The
+   * loops start at the first non-halo cell and stop at the last non-halo cell.
+   * The loop ranges are over the entire dataset, dat_size.
+   */
+  int dat_size[] = {dat->size[0]};
+  int dat_start[] = {(int)abs(dat->d_m[0])};
+  int dat_stop[] = {dat_size[0] - dat->d_p[0]};
+
+  /*
+   * Loop over each dimension, with each loop variable being incremented by the
+   * stride for that dimension.
+   */
+  for (int i = dat_start[0]; i < dat_stop[0]; i += stride_i) {
+    /*
+     * Using `index3d` we can calculate the index in the flat 1d buffers
+     * given the 3d coordinates. The coordinate transformation for the
+     * strided index is rather complex and ugly to look at. The index is
+     * multiplied by the element size of ops_dat, because ops_dat->data is
+     * a char*, so we are working with bytes rather than structured data.
+     */
+    const size_t index_dat = index1d(i, dat->size) * dat->elem_size;
+    const size_t index_stride = index1d((i - dat_start[0]) / stride_i, strided_dat->size) * strided_dat->elem_size;
+    /*
+     * Copy the data from the original dataset into the strided buffer. This
+     * has to be done using memcpy because ops_dat->data is a char* so we
+     * are working in bytes, rather than structured data.
+     */
+    memcpy(&strided_dat->data[index_stride], &dat->data[index_dat], strided_dat->elem_size);
   }
 
-  return new_dat;
+  return strided_dat;
 }
 
-/*
- * Takes an input dataset to create a new strided dataset
- * In this example, we create a new ops_dat with halo cells. The halo cells
- * have been hardcoded, as I expect they would be known at code generation.
+/**
+ * @brief Creates a strided ops_dat, including halo cells, from an existing
+ *        ops_dat.
+ *
+ * @param[in] dat The ops_dat to create a strided ops_dat from
+ * @param[in] stride_i The stride in the i direction
+ * @param[in] stride_j The stride in the j direction
+ * @param[in] stride_k The stride in the k direction
+ *
+ * @returns The strided ops_dat
+ *
+ * @details
+ *
+ * This function creates a strided ops_dat by taking an existing ops_dat and
+ * creating a new ops_dat with the specified stride in each dimension. The same
+ * number of halo cells are used in each dimension, taken from the original
+ * ops_dat, but will contain uninitialized (or zero'd) data. This is done for
+ * consistency and to simplify plotting, as you do not need to worry about
+ * different starting indices and padding when processing the data.
+ *
  */
 ops_dat create_strided_ops_dat_with_halo_cells(ops_dat dat, size_t stride_i) {
-  int d_p[] = {5};
-  int d_m[] = {-5};
-  int new_size[] = {(int)ceil(block0np0 / stride_i)};
+  /*
+   * Determine the size of the strided dat halos and the actual size of the
+   * dataset. This is taken from the original ops_dat, but it could be hardcoded
+   * during code generation since we already know the sizes of the halos and
+   * the blocks.
+   */
+  int strided_d_p[] = {dat->d_p[0] - dat->x_pad};
+  int strided_d_m[] = {dat->d_m[0]};
+  int strided_size[] = {(int)ceil(block0np0 / stride_i)};
 
-  double *_dummy = NULL; /* Note that this has to be the type you want the ops_dat to be */
-  ops_dat new_dat = ops_decl_dat(dat->block, dat->dim, new_size, dat->base, d_m, d_p, _dummy, dat->type, dat->name);
+  /*
+   * Create a new dataset using the sizes above. We need to create a new ops_dat
+   * rather than just a buffer since all the HDF5 functions (which we want to
+   * re-use) expect an ops_dat and not a buffer containing data. Note that we
+   * have a dummy variable. This variable *HAS* to be the type of data which
+   * will be stored in the ops_dat. If it is not, this will allocate an
+   * incorrect amount of data and will also initialise some of the fields, such
+   * as ops_dat->elem_size, incorrectly. Anything wrong there will result in
+   * copy errors and garbage output.
+   */
+  double *dummy = NULL;
+  ops_dat strided_dat = ops_decl_dat(dat->block, dat->dim, strided_size, dat->base, strided_d_m, strided_d_p, dummy,
+                                     dat->type, dat->name);
 
-  int size[] = {dat->size[0]};
-  int offset[] = {(int)abs(dat->d_m[0])};
-  int offset_new[] = {(int)abs(new_dat->d_m[0])};
+  /*
+   * Loop variables to control the start and stop of the loop over each
+   * dimension. These values are based on the size of the original dataset. The
+   * loops start at the first non-halo cell and stop at the last non-halo cell.
+   * The loop ranges are over the entire dataset, dat_size.
+   */
+  int dat_size[] = {dat->size[0]};
+  int dat_start[] = {(int)abs(dat->d_m[0])};
+  int dat_stop[] = {dat_size[0] - dat->d_p[0]};
 
-  for (int i = offset[0]; i < size[0] - dat->d_p[0]; i += stride_i) {
-    const size_t index_dat = i * dat->elem_size;
-    const size_t index_new = ((i - offset[0]) / stride_i + offset_new[0]) * new_dat->elem_size;
-    memcpy(new_dat->data + index_new, dat->data + index_dat, new_dat->elem_size);
+  /*
+   * We also need to consider the offset in the strided buffer. This is because
+   * we need to worry about the halo cells in the negative direction, d_m,
+   * otherwise the final output will be missing data and be in the wrong place.
+   */
+  int offset_in_strided_buf[] = {(int)abs(strided_dat->d_m[0])};
+
+  /*
+   * Loop over each dimension, with each loop variable being incremented by the
+   * stride for that dimension.
+   */
+  for (int i = dat_start[0]; i < dat_stop[0]; i += stride_i) {
+    /*
+     * Using `index3d` we can calculate the index in the flat 1d buffers
+     * given the 3d coordinates. The coordinate transformation for the
+     * strided index is rather complex and ugly to look at. The index is
+     * multiplied by the element size of ops_dat, because ops_dat->data is
+     * a char*, so we are working with bytes rather than structured data.
+     */
+    const size_t index_dat = index1d(i, dat->size) * dat->elem_size;
+    const size_t index_stride =
+        index1d((i - dat_start[0]) / stride_i + offset_in_strided_buf[0], strided_dat->size) * strided_dat->elem_size;
+    /*
+     * Copy the data from the original dataset into the strided buffer. This
+     * has to be done using memcpy because ops_dat->data is a char* so we
+     * are working in bytes, rather than structured data.
+     */
+    memcpy(&strided_dat->data[index_stride], &dat->data[index_dat], strided_dat->elem_size);
   }
 
-  return new_dat;
+  return strided_dat;
 }
--- include/io_strided2d.h
+++ include/io_strided2d.h
@@ -1,34 +1,193 @@
-/*
- * Helper function to convert indices to a flattened index
+/**
+ * @brief Convert between 2D indices and linear indices
+ *
+ * @param[in] i The i index
+ * @param[in] j The j index
+ * @param[in] size The size of each dimension
+ *
+ * @returns The linear index
+ *
  */
 size_t index2d(size_t i, size_t j, int *size) { return i + j * size[0]; }
 
-/*
- * Takes an input dataset to create a new strided dataset
- * In this example, we create a new ops_dat without any halo cells, which
- * significantly simplifies copying data between buffers.
+/**
+ * @brief Creates a strided ops_dat, without halo cells, from an existing
+ *        ops_dat.
+ *
+ * @param[in] dat The ops_dat to create a strided ops_dat from
+ * @param[in] stride_i The stride in the i direction
+ * @param[in] stride_j The stride in the j direction
+ *
+ * @returns The strided ops_dat
+ *
+ * @details
+ *
+ * This function creates a strided ops_dat by taking an existing ops_dat and
+ * creating a new ops_dat with the specified stride in each dimension. There
+ * are no halo cells, because it simplifies copying data between buffers.
+ *
  */
-ops_dat create_strided_ops_dat(ops_dat dat, size_t stride_i, size_t stride_j) {
-  int d_p[] = {0, 0};
-  int d_m[] = {0, 0};
-  int new_size[] = {(int)ceil(block0np0 / stride_i), (int)ceil(block0np1 / stride_j)};
+ops_dat create_strided_ops_dat_without_halo_cells(ops_dat dat, size_t stride_i, size_t stride_j) {
+  /*
+   * Determine the size of the strided dat halos and the actual size of the
+   * dataset. In this case, we do not have any halo cells.
+   */
+  int strided_d_p[] = {0, 0};
+  int strided_d_m[] = {0, 0};
+  int strided_size[] = {
+      (int)ceil(block0np0 / stride_i),
+      (int)ceil(block0np1 / stride_j),
+  };
 
-  double *_dummy = NULL; /* Note that this has to be the type you want the ops_dat to be */
-  ops_dat new_dat = ops_decl_dat(dat->block, dat->dim, new_size, dat->base, d_m, d_p, _dummy, dat->type, dat->name);
+  /*
+   * Create a new dataset using the sizes above. We need to create a new ops_dat
+   * rather than just a buffer since all the HDF5 functions (which we want to
+   * re-use) expect an ops_dat and not a buffer containing data. Note that we
+   * have a dummy variable. This variable *HAS* to be the type of data which
+   * will be stored in the ops_dat. If it is not, this will allocate an
+   * incorrect amount of data and will also initialise some of the fields, such
+   * as ops_dat->elem_size, incorrectly. Anything wrong there will result in
+   * copy errors and garbage output.
+   */
+  double *dummy = NULL;
+  ops_dat strided_dat = ops_decl_dat(dat->block, dat->dim, strided_size, dat->base, strided_d_m, strided_d_p, dummy,
+                                     dat->type, dat->name);
 
-  int size[] = {dat->size[0], dat->size[1]};
-  int offset[] = {(int)abs(dat->d_m[0]), (int)abs(dat->d_m[1])};
-  int offset_new[] = {(int)abs(new_dat->d_m[0]), (int)abs(new_dat->d_m[1])};
+  /*
+   * Loop variables to control the start and stop of the loop over each
+   * dimension. These values are based on the size of the original dataset. The
+   * loops start at the first non-halo cell and stop at the last non-halo cell.
+   * The loop ranges are over the entire dataset, dat_size.
+   */
+  int dat_size[] = {dat->size[0], dat->size[1]};
+  int dat_start[] = {(int)abs(dat->d_m[0]), (int)abs(dat->d_m[1])};
+  int dat_stop[] = {dat_size[0] - dat->d_p[0], dat_size[1] - dat->d_p[1]};
 
-  for (int j = offset[1]; j < size[1] - dat->d_p[1]; j += stride_j) {
-    for (int i = offset[0]; i < size[0] - dat->d_p[0]; i += stride_i) {
+  /*
+   * Loop over each dimension, with each loop variable being incremented by the
+   * stride for that dimension.
+   */
+  for (int j = dat_start[1]; j < dat_stop[1]; j += stride_j) {
+    for (int i = dat_start[0]; i < dat_stop[0]; i += stride_i) {
+      /*
+       * Using `index3d` we can calculate the index in the flat 1d buffers
+       * given the 3d coordinates. The coordinate transformation for the
+       * strided index is rather complex and ugly to look at. The index is
+       * multiplied by the element size of ops_dat, because ops_dat->data is
+       * a char*, so we are working with bytes rather than structured data.
+       */
       const size_t index_dat = index2d(i, j, dat->size) * dat->elem_size;
-      const size_t index_new = index2d((i - offset[0]) / stride_i + offset_new[0],
-                                       (j - offset[1]) / stride_j + offset_new[1], new_dat->size) *
-                               new_dat->elem_size;
-      memcpy(new_dat->data + index_new, dat->data + index_dat, new_dat->elem_size);
+      const size_t index_stride =
+          index2d((i - dat_start[0]) / stride_i, (j - dat_start[1]) / stride_j, strided_dat->size) *
+          strided_dat->elem_size;
+      /*
+       * Copy the data from the original dataset into the strided buffer. This
+       * has to be done using memcpy because ops_dat->data is a char* so we
+       * are working in bytes, rather than structured data.
+       */
+      memcpy(&strided_dat->data[index_stride], &dat->data[index_dat], strided_dat->elem_size);
     }
   }
 
-  return new_dat;
+  return strided_dat;
+}
+
+/**
+ * @brief Creates a strided ops_dat, including halo cells, from an existing
+ *        ops_dat.
+ *
+ * @param[in] dat The ops_dat to create a strided ops_dat from
+ * @param[in] stride_i The stride in the i direction
+ * @param[in] stride_j The stride in the j direction
+ *
+ * @returns The strided ops_dat
+ *
+ * @details
+ *
+ * This function creates a strided ops_dat by taking an existing ops_dat and
+ * creating a new ops_dat with the specified stride in each dimension. The same
+ * number of halo cells are used in each dimension, taken from the original
+ * ops_dat, but will contain uninitialized (or zero'd) data. This is done for
+ * consistency and to simplify plotting, as you do not need to worry about
+ * different starting indices and padding when processing the data.
+ *
+ */
+ops_dat create_strided_ops_dat_with_halo_cells(ops_dat dat, size_t stride_i, size_t stride_j) {
+  /*
+   * Determine the size of the strided dat halos and the actual size of the
+   * dataset. This is taken from the original ops_dat, but it could be hardcoded
+   * during code generation since we already know the sizes of the halos and
+   * the blocks.
+   */
+  int strided_d_p[] = {
+      dat->d_p[0] - dat->x_pad,
+      dat->d_p[1],
+  };
+  int strided_d_m[] = {
+      dat->d_m[0],
+      dat->d_m[1],
+  };
+  int strided_size[] = {
+      (int)ceil(block0np0 / stride_i),
+      (int)ceil(block0np1 / stride_j),
+  };
+
+  /*
+   * Create a new dataset using the sizes above. We need to create a new ops_dat
+   * rather than just a buffer since all the HDF5 functions (which we want to
+   * re-use) expect an ops_dat and not a buffer containing data. Note that we
+   * have a dummy variable. This variable *HAS* to be the type of data which
+   * will be stored in the ops_dat. If it is not, this will allocate an
+   * incorrect amount of data and will also initialise some of the fields, such
+   * as ops_dat->elem_size, incorrectly. Anything wrong there will result in
+   * copy errors and garbage output.
+   */
+  double *dummy = NULL;
+  ops_dat strided_dat = ops_decl_dat(dat->block, dat->dim, strided_size, dat->base, strided_d_m, strided_d_p, dummy,
+                                     dat->type, dat->name);
+
+  /*
+   * Loop variables to control the start and stop of the loop over each
+   * dimension. These values are based on the size of the original dataset. The
+   * loops start at the first non-halo cell and stop at the last non-halo cell.
+   * The loop ranges are over the entire dataset, dat_size.
+   */
+  int dat_size[] = {dat->size[0], dat->size[1]};
+  int dat_start[] = {(int)abs(dat->d_m[0]), (int)abs(dat->d_m[1])};
+  int dat_stop[] = {dat_size[0] - dat->d_p[0], dat_size[1] - dat->d_p[1]};
+
+  /*
+   * We also need to consider the offset in the strided buffer. This is because
+   * we need to worry about the halo cells in the negative direction, d_m,
+   * otherwise the final output will be missing data and be in the wrong place.
+   */
+  int offset_in_strided_buf[] = {(int)abs(strided_dat->d_m[0]), (int)abs(strided_dat->d_m[1])};
+
+  /*
+   * Loop over each dimension, with each loop variable being incremented by the
+   * stride for that dimension.
+   */
+  for (int j = dat_start[1]; j < dat_stop[1]; j += stride_j) {
+    for (int i = dat_start[0]; i < dat_stop[0]; i += stride_i) {
+      /*
+       * Using `index3d` we can calculate the index in the flat 1d buffers
+       * given the 3d coordinates. The coordinate transformation for the
+       * strided index is rather complex and ugly to look at. The index is
+       * multiplied by the element size of ops_dat, because ops_dat->data is
+       * a char*, so we are working with bytes rather than structured data.
+       */
+      const size_t index_dat = index2d(i, j, dat->size) * dat->elem_size;
+      const size_t index_stride = index2d((i - dat_start[0]) / stride_i + offset_in_strided_buf[0],
+                                          (j - dat_start[1]) / stride_j + offset_in_strided_buf[1], strided_dat->size) *
+                                  strided_dat->elem_size;
+      /*
+       * Copy the data from the original dataset into the strided buffer. This
+       * has to be done using memcpy because ops_dat->data is a char* so we
+       * are working in bytes, rather than structured data.
+       */
+      memcpy(&strided_dat->data[index_stride], &dat->data[index_dat], strided_dat->elem_size);
+    }
+  }
+
+  return strided_dat;
 }
