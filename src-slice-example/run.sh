88bc5662b2020ffbc9e52a7fe3886a074f603965
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-11-04 13:51:49 +0000

    bit more clean up

--- src-slice-example/run.sh
+++ src-slice-example/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+NP=4
+
 rm -f *.h5 \
     && ./build.sh \
-    &&  mpirun ./build/slice-example_mpi \
+    &&  mpirun -np $NP ./build/slice-example_mpi \
     && python ../scripts/compare-strided_output.py opensbli_output_000001.h5 K75.h5 2 2 1 \
--- src-standalone-example/run.sh
+++ src-standalone-example/run.sh
@@ -2,5 +2,5 @@
 
 rm -f *.h5 \
     && ./build.sh \
-    && ./build/taylorgreen_seq \
+    && ./build/standalone-example_seq \
     && python ../scripts/compare-standalone-example.py opensbli_output.h5 opensbli_output-strided.h5 2 2 2 --halo_size 5 \
