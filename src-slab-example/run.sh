1a286fbbee34cee700c954c65a960da8f37f689c
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-11-25 14:37:01 +0000

    rename example

--- src-slab-example/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NP=2
-
-rm -f *.h5 \
-    && ./build.sh \
-    && ./build/slab-example_seq
-    # && python ../scripts/compare-slice-example.py opensbli_output_000001.h5 K75.h5 2 2 1 \
