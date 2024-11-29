e2cf1e63e2a371c51d911d94ea3bef5690a8abe6
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2024-11-29 15:54:58 +0000

    fix for trying to run mpi

--- src-slice-example/run-cuda.sh
+++ src-slice-example/run-cuda.sh
@@ -2,5 +2,5 @@
 
 rm -f *.h5 \
     && ./build.sh \
-    && mpirun -n 4 ./build/slice-example_cuda \
+    && ./build/slice-example_cuda \
     && python ../scripts/compare-slice-example.py opensbli_output_000001.h5 K75.h5 2 2 2 \
